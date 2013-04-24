0ab6b246300de927ca98aa5226d46a11c3de34ff
jiangbianliming <510006910@qq.com>
2013-04-24 12:04:28 +0800

    5

--- LifeBlog/userManage/Register.xaml.cs
+++ LifeBlog/userManage/Register.xaml.cs
@@ -17,6 +17,7 @@ namespace LifeBlog
     {
         public static ManualResetEvent allDone = new ManualResetEvent(false);
         private HttpWebRequest request;
+        
         private string RegisterUrl = "http://lifeblog.herokuapp.com/api-root/users/";
         public Register()
         {
@@ -25,71 +26,104 @@ namespace LifeBlog
 
         private void Regist_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-             
-            request = (HttpWebRequest)WebRequest.Create(RegisterUrl);
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.Method = "POST";
-            IAsyncResult r = request.BeginGetRequestStream(new AsyncCallback(readRequest), request);
+            
+            //request = (HttpWebRequest)WebRequest.Create(RegisterUrl);
+            //request.ContentType = "application/x-www-form-urlencoded";
+            //request.Method = "POST";
+            //IAsyncResult r = request.BeginGetRequestStream(new AsyncCallback(readRequest), request);
+            //allDone.WaitOne();
+
+            //IAsyncResult responseR = request.BeginGetResponse(new AsyncCallback(getResponse), request);
+            //allDone.WaitOne();
+            WebClient webClient = new WebClient();
+            webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+            var uri = new Uri(RegisterUrl);
+            string postData = getPostdata();
+            webClient.Headers[HttpRequestHeader.ContentLength] = postData.Length.ToString();
+            webClient.AllowWriteStreamBuffering = true;
+            webClient.Encoding = System.Text.Encoding.UTF8;
+            webClient.UploadStringAsync(uri, "POST", postData.ToString());
+            webClient.UploadStringCompleted += new UploadStringCompletedEventHandler(postComplete);
             allDone.WaitOne();
 
-            IAsyncResult responseR = request.BeginGetResponse(new AsyncCallback(getResponse), request);
-            allDone.WaitOne();
         }
-
-        private void getResponse(IAsyncResult ar)
+       
+        private void postComplete(object sender, UploadStringCompletedEventArgs e)
         {
-            HttpWebRequest request = (HttpWebRequest)ar.AsyncState;
-            WebResponse Response = request.EndGetResponse(ar);
-           
-            Stream response = Response.GetResponseStream();
-            StreamReader streamRead = new StreamReader(response);           
-            Char[] readBuff = new Char[256];
-            int count = streamRead.Read(readBuff, 0, 256);
-          
-            while (count > 0)
+            allDone.Set();
+            HttpWebResponse result = JsonConvert.DeserializeObject<HttpWebResponse>(e.Result);
+            if (result.StatusCode == HttpStatusCode.OK)
             {
-                String outputData = new String(readBuff, 0, count);
-                JsonReader reader = new JsonTextReader(new StringReader(outputData));             
-                Console.WriteLine(reader.Value);
-                count = streamRead.Read(readBuff, 0, 256);
-                
+                NavigationService.Navigate(new Uri("/userManage/success.xaml", UriKind.Relative));
             }
-
-            // Close the Stream Object.
-            Response.Close();
-            response.Close();
-            streamRead.Close();
-            allDone.Set();
-
-
         }
-
-        private void readRequest(IAsyncResult ar)
+        private string getPostdata()
         {
-            HttpWebRequest request = (HttpWebRequest)ar.AsyncState;
-            Stream streamResponse = request.EndGetRequestStream(ar);
-            StringWriter sw = new StringWriter();
-            JsonWriter writer = new JsonTextWriter(sw);
-            writer.WriteStartObject();
-            writer.WritePropertyName("username");
-            writer.WriteValue(UserID.Text);
-            writer.WritePropertyName("password");
-            writer.WriteValue(pass.Text);
-            writer.WritePropertyName("date_of_birth");
-            writer.WriteValue(datepicker.Value);
-            writer.WritePropertyName("email");
-            writer.WriteValue(email.Text);
-            writer.WritePropertyName("first_name");
-            writer.WriteValue(first_name.Text);
-            writer.WritePropertyName("last_name");
-            writer.WriteValue(last_name.Text);
-            writer.WriteEndObject();
-            string jsonText = sw.GetStringBuilder().ToString();
-            byte[] byteArray = Encoding.UTF8.GetBytes(jsonText);
-            streamResponse.Write(byteArray,0,jsonText.Length);
-            writer.Close();
-            streamResponse.Close();
-            allDone.Set();
+            LifeBlog.userManage.User user = new LifeBlog.userManage.User();
+            user.UserName = UserID.Text;
+            user.UserPass = pass.Text;
+            user.UserBirth = datepicker.ValueString;
+            user.UserEmail = email.Text;
+            user.UserFirstName = first_name.Text;
+            user.UserLastName = last_name.Text;
+            string result = JsonConvert.SerializeObject(user);
+            return result;
         }
+        //private void getResponse(IAsyncResult ar)
+        //{
+        //HttpWebRequest request = (HttpWebRequest)ar.AsyncState;
+        //WebResponse Response = request.EndGetResponse(ar);
+
+        //Stream response = Response.GetResponseStream();
+        //StreamReader streamRead = new StreamReader(response);           
+        //Char[] readBuff = new Char[256];
+        //int count = streamRead.Read(readBuff, 0, 256);
+
+        //while (count > 0)
+        //{
+        //    String outputData = new String(readBuff, 0, count);
+        //    JsonReader reader = new JsonTextReader(new StringReader(outputData));             
+        //    Console.WriteLine(reader.Value);
+        //    count = streamRead.Read(readBuff, 0, 256);
+
+        //}
+
+        //// Close the Stream Object.
+        //Response.Close();
+        //response.Close();
+        //streamRead.Close();
+        //allDone.Set();
+
+
+        //}
+
+        //    private void readRequest(IAsyncResult ar)
+        //    {
+        //        HttpWebRequest request = (HttpWebRequest)ar.AsyncState;
+        //        Stream streamResponse = request.EndGetRequestStream(ar);
+        //        StringWriter sw = new StringWriter();
+        //        JsonWriter writer = new JsonTextWriter(sw);
+        //        writer.WriteStartObject();
+        //        writer.WritePropertyName("username");
+        //        writer.WriteValue(UserID.Text);
+        //        writer.WritePropertyName("password");
+        //        writer.WriteValue(pass.Text);
+        //        writer.WritePropertyName("date_of_birth");
+        //        writer.WriteValue(datepicker.Value);
+        //        writer.WritePropertyName("email");
+        //        writer.WriteValue(email.Text);
+        //        writer.WritePropertyName("first_name");
+        //        writer.WriteValue(first_name.Text);
+        //        writer.WritePropertyName("last_name");
+        //        writer.WriteValue(last_name.Text);
+        //        writer.WriteEndObject();
+        //        string jsonText = sw.GetStringBuilder().ToString();
+        //        byte[] byteArray = Encoding.UTF8.GetBytes(jsonText);
+        //        streamResponse.Write(byteArray,0,jsonText.Length);
+        //        writer.Close();
+        //        streamResponse.Close();
+        //        allDone.Set();
+        //    }
+        //}
     }
 }
---
+++ LifeBlog/userManage/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeBlog.userManage
+{
+    class User
+    {
+        public String UserName { get; set; }
+        public String UserPass { get; set; }
+        public String UserEmail { get; set; }
+        public String UserBirth { get; set; }
+        public String UserFirstName { get; set; }
+        public String UserLastName { get; set; }
+    }
+}
